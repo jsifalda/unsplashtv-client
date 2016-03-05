92bdc33e64f09371785a10a33fdbf1d4e52fd5ad
jsifalda <sifalda.jiri@gmail.com>
2016-03-05 11:43:13 +0100

    Add showcase

--- src/application.js
+++ src/application.js
@@ -9,8 +9,61 @@ const CLIENT_ID = 'b9288b9e4913497056fbdd1255c0147b6ed3e8e201811f2f3023f6fd5b9e3
 
 App.onLaunch = function(options) {
   // 1
-  var alert = createAlert('Hello World', 'desc'); //leaving 2nd parameter with an empty string
-  navigationDocument.presentModal(alert);
+  // var alert = createAlert('Hello World', 'desc'); //leaving 2nd parameter with an empty string
+  var showcase = createShowcase()
+  navigationDocument.presentModal(showcase);
+}
+
+let createShowcase = () => {
+
+  let template = `<?xml version="1.0" encoding="UTF-8" ?>
+    <document>
+     <showcaseTemplate mode="showcase">
+        <background>
+           <img src="path to images on your server/Car_Movie_1920x1080.png" />
+        </background>
+        <banner>
+           <title>Scenes</title>
+           <row>
+              <button>
+                 <text>Slideshow</text>
+              </button>
+              <button>
+                 <text>Screensaver</text>
+              </button>
+           </row>
+        </banner>
+        <carousel>
+           <section>
+              <lockup>
+                 <img src="path to images on your server/Car_Movie_453x255_C.png" width="453" height="255" />
+                 <title>Scene 1</title>
+              </lockup>
+              <lockup>
+                <img src="path to images on your server/Car_Movie_500x600.png" width="500" height="600" />
+                <title>Scene 2</title>
+              </lockup>
+              <lockup>
+                 <img src="path to images on your server/Car_Movie_308x308_B.png" width="308" height="308" />
+                 <title>Scene 3</title>
+              </lockup>
+              <lockup>
+                 <img src="path to images on your server/Car_Movie_800x600.png" width="800" height="600" />
+                 <title>Scene 4</title>
+              </lockup>
+           </section>
+        </carousel>
+     </showcaseTemplate>
+  </document>
+  `
+
+  return parseTemplate(template)
+}
+
+let parseTemplate = (template) => {
+  var parser = new DOMParser();
+  var alertDoc = parser.parseFromString(template, 'application/xml');
+  return alertDoc
 }
 
 // 2
@@ -23,9 +76,7 @@ var createAlert = function(title, description) {
         '<description>' + description  + '</description>' +
       '</alertTemplate>' +
     '</document>'
-    var parser = new DOMParser();
-    var alertDoc = parser.parseFromString(alertString, 'application/xml');
-    return alertDoc
+    return parseTemplate(alertString)
 }
 
 function jsonRequest(options) {
